fc510243774ce51f8ad1a394b18eec45c76f5c6b
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-08-07 03:24:03 +0300

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
+/** @format */
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+	const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+	const appClass = isDarkMode ? "App dark" : "App light";
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+	function handleDarkModeClick() {
+		setIsDarkMode((prevMode) => !prevMode);
+	}
+
+	return (
+		<div className={appClass}>
+			<header>
+				<h2>Shopster</h2>
+				<button onClick={handleDarkModeClick}>
+					{isDarkMode ? "Light Mode" : "Dark Mode"}
+				</button>
+			</header>
+			<ShoppingList items={itemData} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Filter.js
@@ -0,0 +1,19 @@
+/** @format */
+
+// src/components/Filter.js
+import React from "react";
+
+function Filter({ onCategoryChange }) {
+	return (
+		<div className='Filter'>
+			<select name='filter' onChange={onCategoryChange}>
+				<option value='All'>All</option>
+				<option value='Dairy'>Dairy</option>
+				<option value='Produce'>Produce</option>
+				<option value='Dessert'>Dessert</option>
+			</select>
+		</div>
+	);
+}
+
+export default Filter;
